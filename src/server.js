11fa2b27329d765d77b9c7ec9470fc2366be8c77
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2022-01-05 16:58:49 -0500

    Feat: Rate limiting, Request and Error handling

--- src/server.js
+++ src/server.js
@@ -2,12 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const openWeather = require("./routes/openweather");
 
 const app = express();
 
+//PORT
 const port = process.env.PORT || 3000;
 
+//Rate Limiting
+const limit = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 5,
+});
+
+// app.use(limit);
+app.set("trust proxy", 1);
+
+//Cors
 app.use(
   cors({
     origin: `${process.env.ORIGIN}`,
@@ -15,7 +27,16 @@ app.use(
   })
 );
 
+//Open Weather API Route
 app.use("/api/weather", openWeather);
+
+app.use((req, res) => {
+  res
+    .status(404)
+    .send({
+      message: "The server has not found anything matching the Request-URI.",
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.........`);
