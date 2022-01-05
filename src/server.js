e04a73345a75b2b69b771b0ac466867772e27796
Abdulsalam O. Odetayo <adeodetayo92@gmail.com>
2022-01-05 18:09:01 -0500

    Feat: Handling onecall search with longitude and latitude. (#2)
    * Feat: Rate limiting, Request and Error handling
    
    * Feat: Handling search with only state name
    
    * Feat: Imporved rate limiting, per day, per2sec
    
    * Feat: Handling onecall search with longitude and latitude.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,32 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const openWeather = require("./routes/openweather");
 
 const app = express();
 
+//PORT
 const port = process.env.PORT || 3000;
 
+//Rate Limiting
+const limitPerDay = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 60,
+  message: "Rate limit exhausted by this IP, please try again after 24 hours.",
+});
+
+const limitPerSec = rateLimit({
+  windowMs: 3 * 1000,
+  max: 1,
+  message: "Too many requests, please try again after a few seconds.",
+});
+
+app.use(limitPerDay);
+app.use(limitPerSec);
+app.set("trust proxy", 1);
+
+//Cors
 app.use(
   cors({
     origin: `${process.env.ORIGIN}`,
@@ -15,7 +35,16 @@ app.use(
   })
 );
 
+//Open Weather API Route
 app.use("/api/weather", openWeather);
+
+app.use((req, res) => {
+  res
+    .status(404)
+    .send({
+      message: "The server has not found anything matching the Request-URI.",
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.........`);
