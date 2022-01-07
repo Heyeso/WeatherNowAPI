d8a2e5da61ddf481069375bd22ba1b7f07afb37f
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2022-01-07 18:58:07 -0500

    Feat: Limit Headers, Test API endpoint

--- src/server.js
+++ src/server.js
@@ -23,8 +23,8 @@ const limitPerSec = rateLimit({
   message: "Too many requests, please try again after a few seconds.",
 });
 
-app.use(limitPerDay);
 app.use(limitPerSec);
+app.use(limitPerDay);
 app.set("trust proxy", 1);
 
 //Cors
@@ -32,18 +32,104 @@ app.use(
   cors({
     origin: `${process.env.ORIGIN}`,
     methods: ["GET"],
+    allowedHeaders: ["X-RateLimit-Limit", "X-RateLimit-Remaining"],
+    exposedHeaders: ["X-RateLimit-Limit", "X-RateLimit-Remaining"],
   })
 );
+
+app.get("/", (req, res) => {
+  res.send({
+    city: "Carney",
+    country: "US",
+    sunrise: 1641558363,
+    sunset: 1641592682,
+    temperature: 271.8,
+    weather: {
+      main: "Snow",
+      description: "snow",
+    },
+    daily: [
+      {
+        temperature: {
+          max: 271.99,
+          min: 266.94,
+        },
+        weather: {
+          main: "Snow",
+          description: "snow",
+        },
+      },
+      {
+        temperature: {
+          max: 271.4,
+          min: 265.18,
+        },
+        weather: {
+          main: "Clear",
+          description: "clear sky",
+        },
+      },
+      {
+        temperature: {
+          max: 277.71,
+          min: 270.04,
+        },
+        weather: {
+          main: "Rain",
+          description: "moderate rain",
+        },
+      },
+      {
+        temperature: {
+          max: 276.55,
+          min: 268.44,
+        },
+        weather: {
+          main: "Clouds",
+          description: "broken clouds",
+        },
+      },
+      {
+        temperature: {
+          max: 268.46,
+          min: 265.87,
+        },
+        weather: {
+          main: "Clouds",
+          description: "scattered clouds",
+        },
+      },
+      {
+        temperature: {
+          max: 272.88,
+          min: 266.21,
+        },
+        weather: {
+          main: "Clouds",
+          description: "broken clouds",
+        },
+      },
+      {
+        temperature: {
+          max: 276.62,
+          min: 271.93,
+        },
+        weather: {
+          main: "Clouds",
+          description: "broken clouds",
+        },
+      },
+    ],
+  });
+});
 
 //Open Weather API Route
 app.use("/api/weather", openWeather);
 
 app.use((req, res) => {
-  res
-    .status(404)
-    .send({
-      message: "The server has not found anything matching the Request-URI.",
-    });
+  res.status(404).send({
+    message: "The server has not found anything matching the Request-URI.",
+  });
 });
 
 app.listen(port, () => {
